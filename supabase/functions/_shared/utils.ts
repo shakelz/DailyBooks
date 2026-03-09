128666a2ce5ae027b7e120bbdc1b0393d5f0085a
shakelz <mohammedshakeel8801@gmail.com>
2026-03-09 22:33:20 +0100

    Allow admin function secret header in CORS

--- supabase/functions/_shared/utils.ts
+++ supabase/functions/_shared/utils.ts
@@ -2,7 +2,7 @@ import { createClient } from 'jsr:@supabase/supabase-js@2'
 
 export const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-admin-secret',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
   'Access-Control-Max-Age': '86400',
 }
